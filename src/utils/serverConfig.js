6cddd4265a9747a461fff6ad5abe5fec718e54a8
hexzoner <hexzon@gmail.com>
2024-08-07 20:04:05 +0200

    change name and send score to backend

---
+++ src/utils/serverConfig.js
@@ -0,0 +1,6 @@
+const serverConfig = {
+  serverUri: import.meta.env.VITE_SERVER_URI,
+  serverPort: import.meta.env.VITE_SERVER_PORT,
+};
+
+export default serverConfig;
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,4 +1,5 @@
 const rosterKey = "pokemon-roster";
+const nameKey = "pokemon-username";
 
 export function getRoster() {
   return JSON.parse(localStorage.getItem(rosterKey)) || [];
@@ -6,4 +7,12 @@ export function getRoster() {
 
 export function saveRoster(roster) {
   localStorage.setItem(rosterKey, JSON.stringify(roster));
+}
+
+export function saveUsername(name) {
+  localStorage.setItem(nameKey, name);
+}
+
+export function loadUsername() {
+  return localStorage.getItem(nameKey) || "Player";
 }
