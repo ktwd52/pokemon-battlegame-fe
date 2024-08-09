44d72e2b89594f86a27df5ab254e1094216822cb
hexzoner <hexzon@gmail.com>
2024-08-09 14:00:11 +0200

    weakness/type in combat

--- src/utils/temporaryPokemons.js
+++ src/utils/temporaryPokemons.js
@@ -53,6 +53,22 @@ export const dummyPokemon1 = {
       },
     },
   ],
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: "grass",
+        url: "https://pokeapi.co/api/v2/type/12/",
+      },
+    },
+    {
+      slot: 2,
+      type: {
+        name: "poison",
+        url: "https://pokeapi.co/api/v2/type/4/",
+      },
+    },
+  ],
 };
 
 export const dummyPokemon2 = {
